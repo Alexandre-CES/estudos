140fde58132e930c1bd36722c24b8986e496963e
Alexandre-CES <147560376+Alexandre-CES@users.noreply.github.com>
2024-02-24 16:04:31 -0300

    posts

--- node_js/mongodb/routes/admin.js
+++ node_js/mongodb/routes/admin.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const mongoose = require('mongoose')
 require('../models/Category')
 const Category = mongoose.model('categories')
+require('../models/Post')
 
 router.get('/', (req,res) => {
     res.render('admin/index')
@@ -55,11 +56,58 @@ router.post('/categories/new', (req, res) => {
 
 router.get('/categories/edit/:id', (req, res) => {
     Category.findOne({_id: req.params.id}).lean().then((category) =>{
-        res.render('admin/editcategories', {name: category.name, slug: category.slug})
+        res.render('admin/editcategories', {name: category.name, slug: category.slug, _id: category._id})
     }).catch((err) => {
         req.flash('error_msg', `there was an error searching the category: ${err}`)   
         res.redirect('/admin/categories')
     })
+})
+
+router.post('/categories/edit/:id', (req, res) => {
+    Category.findOneAndUpdate({_id: req.params.id}, {name: req.body.name, slug: req.body.slug}).then(()=>{
+        req.flash('success_msg', 'Category updated successfully')
+        res.redirect('/admin/categories')
+    }).catch((err)=>{
+        req.flash('error_msg', 'error updating category')
+        res.redirect('/admin/categories')
+    })
+})
+
+router.post('/categories/delete', (req, res) => {
+    Category.findOneAndDelete({_id: req.body.id}).then(()=>{
+        req.flash('success_msg', 'Category deleted successfully')
+        res.redirect('/admin/categories')
+    }).catch((err)=>{
+        req.flash('error_msg', 'error deleting category')
+        res.redirect('/admin/categories')
+    })
+})
+
+router.get('/posts', (req, res) => {
+    res.render('admin/posts')
+})
+
+router.get('/posts/add', (req,res)=> {
+    Category.find().lean().then((categories) =>{
+        res.render('admin/addposts', {categories:categories})
+    }).catch((err) => {
+        req.flash('error_msg', 'Ops..:')
+        res.redirect('/admin/posts')
+    })
+})
+
+router.post('/posts/new', (req, res) => {
+    var errors = []
+
+    if(req.body.category == '0'){
+        errors.push({text: 'invalid category'})
+    }
+
+    if(errors.length > 0){
+        res.render('admin/addposts', {errors: errors})
+    }else{
+
+    }
 })
 
 module.exports = router
