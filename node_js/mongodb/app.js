fdfb01672493743ced7c7cbc34c0bfefd83b5a38
Alexandre-CES <147560376+Alexandre-CES@users.noreply.github.com>
2024-02-24 10:30:45 -0300

    nem sei

--- node_js/mongodb/app.js
+++ node_js/mongodb/app.js
@@ -3,9 +3,25 @@
     const handlebars = require('express-handlebars')
     const app = express()
     const admin = require('./routes/admin')
-    //const mongoose = require('mongoose')
+    const path = require('path')
+    const mongoose = require('mongoose')
+    const session = require('express-session')
+    const flash = require('connect-flash')
 
 //Configuration
+    //Sessão
+        app.use(session({
+            secret: 'segredosecreto',
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash())
+    //Middleware
+        app.use((req,res,next)=>{
+            res.locals.success_msg = req.flash('success_msg')
+            res.locals.error_msg = req.flash('error_msg')
+            next()
+        })
     //Body Parser
         app.use(express.urlencoded({extended: true}))
         app.use(express.json())
@@ -13,7 +29,20 @@
         app.engine('handlebars', handlebars.engine({defaultLayout:'main'}))
         app.set('view engine', 'handlebars')
     //Mongoose
-        //coming Soon
+        mongoose.Promise = global.Promise
+        mongoose.connect('mongodb://127.0.0.1:27017/blogapp').then(() =>{
+            console.log('Mongo connected')
+        }).catch((err) =>{
+            console.log(`Error: ${err}`)
+        })
+    //Public
+    app.use(express.static(path.join(__dirname, 'public')))
+
+    app.use((req, res, next) =>{
+        console.log('Loading')
+        next()
+    })
+
 //Routes
     app.use('/admin', admin)
 
--- node_js/mongodb/routes/admin.js
+++ node_js/mongodb/routes/admin.js
@@ -1,16 +1,65 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+require('../models/Category')
+const Category = mongoose.model('categories')
 
 router.get('/', (req,res) => {
-    res.send('main')
-})
-
-router.get('/posts', (req,res) => {
-    res.send('posts')
+    res.render('admin/index')
 })
 
 router.get('/categories', (req,res) => {
-    res.send('categories')
+    Category.find().sort({date: 'desc'}).lean().then((categories) =>{
+        res.render('admin/categories', {categories: categories})
+    }).catch((err) => {
+        req.flash('error_msg', `there was an error listing the categories: ${err}`)   
+        res.redirect('/admin')
+    })
+})
+
+router.get('/categories/add', (req,res) => {
+    res.render('admin/addcategories')
+})
+
+router.post('/categories/new', (req, res) => {
+
+    var errors = []
+
+    if(!req.body.name || typeof req.body.name == undefined || req.body.name == null){
+        errors.push({text: 'invalid name'})
+    }
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        errors.push({text: 'invalid slug'})
+    }
+
+    if(req.body.name.length < 2){
+        errors.push({text: `small category's name`})
+    }
+
+    if(errors.length > 0){
+        res.render('admin/addcategories', {errors: errors})
+    }else{
+        const NewCategory = {
+            name: req.body.name,
+            slug: req.body.slug
+        }
+        new Category(NewCategory).save().then(()=>{
+            req.flash('success_msg', 'Category created successfully')
+            res.redirect('/admin/categories')
+        }).catch((err)=>{
+            req.flash('error_msg', 'error saving category')
+            res.redirect('/admin/categories')
+        })
+    }
+})
+
+router.get('/categories/edit/:id', (req, res) => {
+    Category.findOne({_id: req.params.id}).lean().then((category) =>{
+        res.render('admin/editcategories', {name: category.name, slug: category.slug})
+    }).catch((err) => {
+        req.flash('error_msg', `there was an error searching the category: ${err}`)   
+        res.redirect('/admin/categories')
+    })
 })
 
 module.exports = router
