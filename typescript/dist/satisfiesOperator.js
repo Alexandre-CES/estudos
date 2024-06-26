b180cc6170ae1cc3b1e513a4dc6360352eb5c4c9
Alexandre-CES <147560376+Alexandre-CES@users.noreply.github.com>
2024-06-25 21:09:06 -0300

    satisfies operator

--- typescript/dist/satisfiesOperator.js
+++ typescript/dist/satisfiesOperator.js
@@ -1,1 +1,16 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const pessoa = {
+    localNascimento: 'São Paulo',
+    residenciaAtual: { x: 10, y: 20 }
+};
+console.log(pessoa.localNascimento.toUpperCase());
+const config = {
+    host: 'localhost',
+    port: 3306,
+    tryReconnect: () => true,
+};
+function Connect() {
+    let { host, port, tryReconnect } = config;
+    createConnection(host, `${port}`, tryReconnect(), 10);
+}
