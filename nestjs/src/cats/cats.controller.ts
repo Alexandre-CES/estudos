6f1af19b4c79180712383b0b1d853563873f3fd9
Alexandre-CES <alexandreces666@gmail.com>
2024-07-02 09:57:10 -0300

    exception

--- nestjs/src/cats/cats.controller.ts
+++ nestjs/src/cats/cats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, HttpCode, Header, Redirect, Param, Body } from '@nestjs/common';
+import { Controller, Get, Post, HttpCode, Header, Redirect, Param, Body, HttpException, HttpStatus } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
@@ -24,6 +24,11 @@ export class CatsController{
   @Get('ab*cd')
   wildGet(){
     return 'this routes uses a wildcard';
+  }
+
+  @Get('error')
+  throwError(){
+    throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
   }
 
   @Post('create')
