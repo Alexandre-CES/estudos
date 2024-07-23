b0343cac3849b8b0bad5ba8df1cb50c8781cc637
Alexandre-CES <147560376+Alexandre-CES@users.noreply.github.com>
2024-07-22 21:39:58 -0300

    starting calculator

---
+++ typescript/calculator/app.ts
@@ -0,0 +1,9 @@
+
+
+const result = document.getElementById('result');
+
+function insert(num:string){
+    if (result){
+        result.innerHTML += num;
+    }
+}
---
+++ typescript/calculator/dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+const result = document.getElementById('result');
+function insert(num) {
+    if (result) {
+        result.innerHTML += num;
+    }
+}
