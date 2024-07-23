5edae2179bdbdd65478db4960bb6ca9d1a11df7c
Alexandre-CES <147560376+Alexandre-CES@users.noreply.github.com>
2024-07-23 15:54:23 -0300

    starting logic and css
    -starting to looks like a calculator
    -functions of operations
    -new clear buttons(already working)

--- typescript/calculator/app.ts
+++ typescript/calculator/app.ts
@@ -1,9 +1,54 @@
 
 
 const result = document.getElementById('result');
+let storedNumber:string = '0';
 
+//clear everything
+function clearAll(){
+    if(result){
+        result.innerHTML = '';
+    }
+}
+
+//clear last digit
+function clearOne() {
+    if (result) {
+        result.innerHTML = result.innerHTML.slice(0, -1);
+    }
+}
+
+//insert number
 function insert(num:string){
-    if (result){
+    if(result){
         result.innerHTML += num;
     }
+}
+
+//to store a number so you can put another
+function storeNumber(num:string){
+    storedNumber = num;
+}
+
+function calculate(ope:string){
+    if(result){
+        if(result.innerHTML.trim() == ''){
+            result.innerHTML = storedNumber;
+        }
+    }
+}
+
+function divide(x:number, y:number): number{
+    return x / y
+}
+
+function multiply(x:number, y:number): number{
+    return x * y
+}
+
+function subtract(x:number, y:number): number{
+    return x - y
+}
+
+function sum(x:number, y:number): number{
+    return x - y
 }
--- typescript/calculator/dist/app.js
+++ typescript/calculator/dist/app.js
@@ -1,7 +1,44 @@
 "use strict";
 const result = document.getElementById('result');
+let storedNumber = '0';
+//clear everything
+function clearAll() {
+    if (result) {
+        result.innerHTML = '';
+    }
+}
+//clear last digit
+function clearOne() {
+    if (result) {
+        result.innerHTML = result.innerHTML.slice(0, -1);
+    }
+}
+//insert number
 function insert(num) {
     if (result) {
         result.innerHTML += num;
     }
+}
+//to store a number so you can put another
+function storeNumber(num) {
+    storedNumber = num;
+}
+function calculate(ope) {
+    if (result) {
+        if (result.innerHTML.trim() == '') {
+            result.innerHTML = storedNumber;
+        }
+    }
+}
+function divide(x, y) {
+    return x / y;
+}
+function multiply(x, y) {
+    return x * y;
+}
+function subtract(x, y) {
+    return x - y;
+}
+function sum(x, y) {
+    return x - y;
 }
