44bb795a7ed838d4af8e1ca24dd6f9cf4e905b30
Alexandre-CES <147560376+Alexandre-CES@users.noreply.github.com>
2025-01-08 22:56:32 -0300

    feat: authentication with email and password

--- node_js/firebase/firebase-module.js
+++ node_js/firebase/firebase-module.js
@@ -8,17 +8,15 @@ import {
     getDocs,
     doc,
     updateDoc,
-    deleteDoc 
+    deleteDoc
 } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
-import { 
-    API_KEY,
-    AUTH_DOMAIN,
-    PROJECT_ID,
-    STORAGE_BUCKET,
-    MESSAGING_SENDER_ID,
-    APP_ID,
-    MEASUREMENT_ID 
-} from "./infos.js";
+import{
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    updateProfile
+} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -26,32 +24,48 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: API_KEY,
-    authDomain: AUTH_DOMAIN,
-    projectId: PROJECT_ID,
-    storageBucket: STORAGE_BUCKET,
-    messagingSenderId: MESSAGING_SENDER_ID,
-    appId: APP_ID,
-    measurementId: MEASUREMENT_ID
-};
+    apiKey: "AIzaSyDaryRbdZF52F9XjGZedkwyEHNpEp3dIGE",
+    authDomain: "teste-77185.firebaseapp.com",
+    projectId: "teste-77185",
+    storageBucket: "teste-77185.firebasestorage.app",
+    messagingSenderId: "310872162520",
+    appId: "1:310872162520:web:9ab770f77496c6fd8897ad",
+    measurementId: "G-8WWZD05PH2"
+  };
 
 // *Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
 const db = getFirestore(app);
 
+//* => html elements
+
+//comment list / feedback
 const commentList = document.getElementById('comment-list');
 
-const buttonGetUsers = document.getElementById('users');
+//test buttons
+const buttonCurrentUser = document.getElementById('current-user');
 
-const inputUserId = document.getElementById('user-id');
+//add new comment elements
 const inputStars = document.getElementById('stars');
 const inputComment = document.getElementById('comment');
 const buttonNewComment = document.getElementById('new-comment');
 
+//register / login / logout
+const inputDisplayName = document.getElementById('displayName');
+const inputEmail = document.getElementById('email');
+const inputPassword = document.getElementById('password');
+const buttonRegister = document.getElementById('register');
+const buttonLogin = document.getElementById('login');
+const buttonLogout = document.getElementById('logout');
+
 // * => functions
 
-//update comments:
+/*
+    *load list of comments inside list
+    updateComments():void
+*/
 async function updateComments(){
     const querySnapshot = await getFeeds();
 
@@ -74,57 +88,64 @@ async function updateComments(){
 
 updateComments();
 
-//return list of users 
-async function getUsers() {
-    try {
-        const querySnapshot = await getDocs(collection(db, 'users'));
-        querySnapshot.forEach((doc) => {
-            console.log(`${doc.id} =>`, doc.data());
-        });
-    } catch (err) {
-        console.error('Error searching for users:', err);
-    }
-}
-
-//return list of feedbacks
+/*
+    *return list of feedbacks
+    getFeeds(): Promise<{comment:string, stars:number, id_user:string}[] | {comment:string}[]>
+*/
 async function getFeeds() {
     try {
         const querySnapshot = await getDocs(collection(db, 'feedback'));
         return querySnapshot;
     } catch (err) {
-        return ['Error searching for comments:', err];
+        return [{ comment:`Error searching for comments: ${err}` }];
     }
 }
 
-//add new comment in feedback
+/*
+    * add new comment in feedback
+
+    newComment():void
+*/
 async function newComment(user_id, stars, comment){
     try{
-        const docRef  = await addDoc(collection(db, "feedback"),{
+        const docRef  = await addDoc(collection(db, 'feedback'),{
             id_user:user_id,
             stars:stars,
             comment:comment
         });
         console.log('success adding comment: ', docRef.id);
     }catch(err){
-        console.error("Error adding comment:", err);
+        console.error('Error adding comment:', err);
     }
 }
 
-//* read buttons
-buttonGetUsers.addEventListener("click", () => {
-    getUsers();
+//* => button event listeners
+
+//log current user
+buttonCurrentUser.addEventListener('click', ()=>{
+    const user = auth.currentUser;
+    if (user) {
+        
+        console.log('user:', user);
+        console.log('UID:', user.uid);
+        console.log('E-mail:', user.email);
+        console.log('name:', user.displayName);
+
+      } else {
+        console.log('No user logged.');
+      }
 });
 
-//* add buttons
-buttonNewComment.addEventListener("click", () => {
+// add new comment
+buttonNewComment.addEventListener('click', () => {
     try{
         const userId = inputUserId.value;
         const stars = inputStars.value;
         const comment = inputComment.value;
 
-        if(!userId | !stars | !comment){
+        if(!userId || !stars || !comment){
             console.error('invalid fields');
-            throw new error('invalid fields');
+            throw new Error('invalid fields');
         }else{
             newComment(userId,stars,comment);
         }
@@ -134,3 +155,52 @@ buttonNewComment.addEventListener("click", () => {
     }
 });
 
+//register
+buttonRegister.addEventListener('click', async ()=>{
+    try{
+        const displayName = inputDisplayName.value;
+        const email = inputEmail.value;
+        const password = inputPassword.value;
+
+        if(!email || !password){
+            console.error('invalid fields');
+            throw new Error('invalid fields');
+        }else{
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+            if(displayName){
+                const user = userCredential.user;
+                await updateProfile(user, {
+                    displayName: displayName,
+                  });
+            }
+
+            console.log('registered user:', userCredential.user);
+        }
+    }catch(err){
+        console.error('error on register: ', err);
+    }
+});
+
+//login
+buttonLogin.addEventListener('click', async () => {
+    try {
+        const email = inputEmail.value;
+        const password = inputPassword.value;
+
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        console.log('logged user: ', userCredential.user);
+    } catch (err) {
+        console.error('error on login: ', err);
+    }
+});
+
+//logout
+buttonLogout.addEventListener('click', async () => {
+    try {
+        await signOut(auth);
+        console.log('logout');
+    } catch (error) {
+        console.error('error on logout: ', error);
+    }
+});
