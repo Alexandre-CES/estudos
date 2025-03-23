38ca9a441726809241dc6ba7bbc9b0893f8ec0ce
Alexandre <alexandreces666@gmail.com>
2025-03-23 15:52:56 -0300

    learning more

---
+++ java/spring/first-spring-app/src/main/java/com/first/first_spring_app/configuration/HelloConfiguration.java
@@ -0,0 +1,15 @@
+package com.first.first_spring_app.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloConfiguration {
+
+    /*
+    @Bean
+    public SDKAWS sdkAWS(){
+        return new SDKAWS();
+    }
+    */
+}
--- java/spring/first-spring-app/src/main/java/com/first/first_spring_app/controller/HelloWorldController.java
+++ java/spring/first-spring-app/src/main/java/com/first/first_spring_app/controller/HelloWorldController.java
@@ -1,16 +1,33 @@
 package com.first.first_spring_app.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;;
+
+import com.first.first_spring_app.domain.User;
+import com.first.first_spring_app.service.HelloWorldService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController // =  @Controller + @ResponseBody
 @RequestMapping("/hello-world") //Endpoint /hello-world
 public class HelloWorldController {
     
+    @Autowired
+    private HelloWorldService helloWorldService;
+    
     @GetMapping //endpoint /
     public String helloWorld(){
-        return "Hello World";
+        return this.helloWorldService.helloWorld("Alexandre");
     }
     
+    @PostMapping("/{id}")
+    public String helloWorldPost(@PathVariable("id") String id, @RequestParam(value = "filter", defaultValue = "none") String filter, @RequestBody User body){
+        return "Hello, World " + body.getName() + " : " + id + " : " + filter;
+    }
 }
