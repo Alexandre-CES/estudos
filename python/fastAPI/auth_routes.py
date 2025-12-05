b2fc29af649c6c5195593d3384418f7c9712daa9
Alexandre <alexandreces666@gmail.com>
2025-12-05 13:39:14 -0300

    create order and login

--- python/fastAPI/auth_routes.py
+++ python/fastAPI/auth_routes.py
@@ -2,10 +2,14 @@ from fastapi import APIRouter, Depends, HTTPException
 from models import User
 from dependencies import get_session
 from main import bcrypt_context
-from schemas import UserSchema
+from schemas import UserSchema, LoginSchema
 from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix='/auth', tags=['auth'])
+
+def create_token(id_user):
+    token = f'oasrf2kmsak{id_user}'
+    return token
 
 @auth_router.get('/')
 async def autenticar():
@@ -23,3 +27,15 @@ async def create_account(user_schema: UserSchema, session: Session = Depends(get
         session.add(new_user)
         session.commit()
         return {'message':f'User created successfully: {user_schema.email}'}
+
+@auth_router.post('/login')
+async def login(login_schema: LoginSchema, session: Session = Depends(get_session)):
+    user = session.query(User).filter(User.email==login_schema.email).first()
+    if not user:
+        raise HTTPException(status_code=400, detail='User not found')
+    else:
+        access_token = create_token(user.id)
+        return {
+            'access_token': access_token,
+            'token_type': 'Bearer'
+        }
--- python/fastAPI/order_routes.py
+++ python/fastAPI/order_routes.py
@@ -1,7 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends
+from sqlalchemy.orm import Session
+from dependencies import get_session
+from schemas import OrderSchema
+from models import Order
 
 order_router = APIRouter(prefix='/order', tags=['order'])
 
 @order_router.get('/')
-async def pedidos():
+async def orders():
     return {'message': 'acessou rota de pedidos'}
+
+@order_router.post('/order')
+async def create_order(orderSchema: OrderSchema, session: Session = Depends(get_session)):
+    new_order = Order(user=orderSchema.user_id)
+    session.add(new_order)
+    session.commit()
+    return {'message': f'Order created successfully. Order ID: {new_order.id}'}
