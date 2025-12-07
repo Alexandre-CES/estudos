8f0339025bbe5ef50fcda6fb58ddfba3c60202f1
Alexandre <alexandreces666@gmail.com>
2025-12-07 15:12:49 -0300

    cancel order

--- python/fastAPI/order_routes.py
+++ python/fastAPI/order_routes.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter,Depends, HTTPException
 from sqlalchemy.orm import Session
-from dependencies import get_session
+from dependencies import get_session, verify_token
 from schemas import OrderSchema
-from models import Order
+from models import Order, User
 
-order_router = APIRouter(prefix='/order', tags=['order'])
+order_router = APIRouter(prefix='/order', tags=['order'], dependencies=[Depends(verify_token)])
 
 @order_router.get('/')
 async def orders():
@@ -16,3 +16,20 @@ async def create_order(orderSchema: OrderSchema, session: Session = Depends(get_
     session.add(new_order)
     session.commit()
     return {'message': f'Order created successfully. Order ID: {new_order.id}'}
+
+@order_router.post('/order/cancel/{order_id}')
+async def cancel_order(order_id: int, session: Session = Depends(get_session), user: User = Depends(verify_token)):
+    #user.admin = True
+    #user.id = Order.user
+    order = session.query(Order).filter(Order.id==order_id).first()
+    if not order:
+        raise HTTPException(status_code=400, detail='order not found')
+    if not user.admin and user.id != order.user:
+        raise HTTPException(status_code=401, detail='You can not make this operation')
+    else:
+        order.status = "CANCELED"
+        session.commit()
+        return{
+            'message': f'order {order.id} canceled successfully',
+            'order': order
+        }
